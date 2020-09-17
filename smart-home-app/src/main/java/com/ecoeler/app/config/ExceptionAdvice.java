c45e7a9862f929371b25fedd7fccc51a887d86eb
Administrator <415456407@qq.com>
2020-09-17 14:53:29 +0800

    [feat] home-web 认证 授权 功能完成

--- smart-home-app/src/main/java/com/ecoeler/app/config/ExceptionAdvice.java
+++ smart-home-app/src/main/java/com/ecoeler/app/config/ExceptionAdvice.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @since 2020-09-08
  **/
 @ControllerAdvice
-public class ExceptionCatch {
+public class ExceptionAdvice {
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseBody
--- smart-home-web/src/main/java/com/ecoeler/web/config/ExceptionAdvice.java
+++ smart-home-web/src/main/java/com/ecoeler/web/config/ExceptionAdvice.java
@@ -1,18 +1,11 @@
 package com.ecoeler.web.config;
 
-import com.ecoeler.exception.ServiceException;
-import com.ecoeler.model.code.CommonCode;
-import com.ecoeler.model.code.ResultCode;
 import com.ecoeler.model.code.TangCode;
 import com.ecoeler.model.response.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
 
 
 /**
@@ -24,7 +17,7 @@ import java.util.HashMap;
  * @since 2020-09-08
  **/
 @ControllerAdvice
-public class ExceptionCatch {
+public class ExceptionAdvice {
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseBody
